0217484ca1f59d309d870cee759ffbab2a992d09
gichimux <gichimumwai1@gmail.com>
2019-05-13 16:09:34 +0300

    initialized bootstrap extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from .config import DevConfig
 
 #initializing application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
+
+#setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile("config.py")
+
+#initializing flask extensions
+bootstrap = Bootstrap(app)
 
 from app import views
+from app import error
---
+++ app/movie_test.py
@@ -0,0 +1,19 @@
+import unittest
+from models import movie
+Movie = movie.Movie
+class MovieTest(unittest.TestCase):
+    '''
+    test class to test the behavior of the movie class
+    '''
+
+    def setUp(self):
+        '''
+        set up method that will run before every test
+        '''
+        self.new_movie = Movie(1234, 'Python must be crazy', 'A thrilling new python series', 'https://image.tmdb.org/t/p/w500/khsjha27hbs', 8.5,129993)
+
+        def test_instance(self):
+            self.assertTrue(isinstance(self.new_movie, Movie))
+
+    if if __name__ == "__main__":
+        unittest.main()
